941ac7e4f5252d8b95f53f5189290ac70db82e26
liam <ljsimpkin@gmail.com>
2023-12-01 20:48:14 +1300

    Added a new method `get_result` to calculate the sum of the first and last numbers in a list of strings.

--- 1/a.rb
+++ 1/a.rb
@@ -13,3 +13,9 @@ end
 def strings_to_numbers(strings)
   strings.map(&:to_i)
 end
+
+def get_result(strings)
+  numbers = get_first_and_last_numbers(strings)
+  sum_numbers(strings_to_numbers(numbers))
+end
+
