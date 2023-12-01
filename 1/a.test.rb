e74f5bed35aef539ae4cb6f85dc2d5ba2949a28a
liam <ljsimpkin@gmail.com>
2023-12-01 19:41:53 +1300

    aider: Add a test case for the function get_first_and_last_numbers that returns the first and last number for each string.
    # Aider chat conversation:
    
    USER: create a test for a function that gets a list of random string values and returns only the first and last number for each string
    
    ASSISTANT: To create a test for a function that gets a list of random string values and returns only the first and last number for each string, we need to modify `1/a.test.rb`:
    
    1. Add a test case for the function.
    
    Here is the *SEARCH/REPLACE* block:
    
    1/a.test.rb
    ```ruby
    <<<<<<< SEARCH
    require 'rspec'
    
    RSpec.describe 'True' do
      it 'equals true' do
        expect(true).to eq(true)
      end
    end
    =======
    require 'rspec'
    
    RSpec.describe 'True' do
      it 'equals true' do
        expect(true).to eq(true)
      end
    end
    
    RSpec.describe 'get_first_and_last_numbers' do
      it 'returns the first and last number for each string' do
        input = ['123abc456', '789def012', '345ghi678']
        output = [['123', '456'], ['789', '012'], ['345', '678']]
        expect(get_first_and_last_numbers(input)).to eq(output)
      end
    end
    >>>>>>> REPLACE
    ```
    
    Please note that this test assumes the existence of a function named `get_first_and_last_numbers` that takes a list of strings as input and returns a list of lists, where each sublist contains the first and last number from the corresponding string in the input list. If the function is named differently or has a different behavior, the test needs to be adjusted accordingly.

--- 1/a.test.rb
+++ 1/a.test.rb
@@ -5,3 +5,11 @@ RSpec.describe 'True' do
     expect(true).to eq(true)
   end
 end
+
+RSpec.describe 'get_first_and_last_numbers' do
+  it 'returns the first and last number for each string' do
+    input = ['123abc456', '789def012', '345ghi678']
+    output = [['123', '456'], ['789', '012'], ['345', '678']]
+    expect(get_first_and_last_numbers(input)).to eq(output)
+  end
+end
