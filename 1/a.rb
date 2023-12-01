6450fa6b9937f87b51fd5ca37fdceabe247a4d0b
liam <ljsimpkin@gmail.com>
2023-12-01 19:43:41 +1300

    aider: Create function get_first_and_last_numbers in a.rb.
    # Aider chat conversation:
    
    USER: awesome now create a function called get_first_and_last_numbers inside a.rb
    
    ASSISTANT: To create a function called `get_first_and_last_numbers` inside `a.rb`, we need to modify `1/a.rb`:
    
    1. Define a function named `get_first_and_last_numbers` that takes a list of strings as input.
    2. For each string in the input list, find the first and last number and add them to a new list.
    3. Return the new list.
    
    Here is the *SEARCH/REPLACE* block:
    
    1/a.rb
    ```ruby
    <<<<<<< SEARCH
    =======
    def get_first_and_last_numbers(strings)
      strings.map do |string|
        first_number = string[/\d+/]
        last_number = string[/\d+(?=\D*$)/]
        [first_number, last_number]
      end
    end
    >>>>>>> REPLACE
    ```
    
    Please note that this function uses regular expressions to find the first and last number in each string. The regular expression `\d+` matches one or more digits, and the regular expression `\d+(?=\D*$)` matches one or more digits that are followed by zero or more non-digits at the end of the string. If the strings can contain numbers in a different format, the regular expressions need to be adjusted accordingly.

---
+++ 1/a.rb
@@ -0,0 +1,7 @@
+def get_first_and_last_numbers(strings)
+  strings.map do |string|
+    first_number = string[/\d+/]
+    last_number = string[/\d+(?=\D*$)/]
+    [first_number, last_number]
+  end
+end
