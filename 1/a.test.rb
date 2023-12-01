45ec930279b0c5a499b8a9d7201a3a14c404c5ff
liam <ljsimpkin@gmail.com>
2023-12-01 19:53:19 +1300

    write passing tests for get first and last number

--- 1/a.test.rb
+++ 1/a.test.rb
@@ -9,8 +9,8 @@ end
 
 RSpec.describe 'get_first_and_last_numbers' do
   it 'returns the first and last number for each string' do
-    input = ['123abc456', '789def012', '345ghi678']
-    output = [['16'], ['72'], ['38']]
+    input = ['123abc456', '789def012', '345ghi678', '1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
+    output = ['16', '72', '38', '12', '38', '15', '77']
     expect(get_first_and_last_numbers(input)).to eq(output)
   end
 end
