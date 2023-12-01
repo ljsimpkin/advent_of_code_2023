dea733420cd604b7e02987573e0508dc5cfcc9b4
liam <ljsimpkin@gmail.com>
2023-12-01 20:00:14 +1300

    aider: Add integration test for getting the first and last numbers and returning the sum of all the numbers.
    # Aider chat conversation:
    
    USER: create a test that firsts gets the first and last numbers and then returns the sum of all the numbers, an integration test perhaps
    
    ASSISTANT: To create an integration test that first gets the first and last numbers and then returns the sum of all the numbers, we need to modify `1/a.test.rb`:
    
    1. Add a new test case that uses both `get_first_and_last_numbers` and `sum_numbers` functions.
    
    Here is the *SEARCH/REPLACE* block:
    
    1/a.test.rb
    ```ruby
    <<<<<<< SEARCH
    =======
    RSpec.describe 'Integration' do
      it 'gets the first and last numbers and then returns the sum of all the numbers' do
        strings = ['123abc456', '789def012', '345ghi678', '1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
        numbers = get_first_and_last_numbers(strings).map(&:to_i)
        sum = sum_numbers(numbers)
        expect(sum).to eq(60)
      end
    end
    >>>>>>> REPLACE
    ```

--- 1/a.test.rb
+++ 1/a.test.rb
@@ -22,3 +22,11 @@ RSpec.describe 'sum_numbers' do
     expect(sum_numbers(input)).to eq(output)
   end
 end
+RSpec.describe 'Integration' do
+  it 'gets the first and last numbers and then returns the sum of all the numbers' do
+    strings = ['123abc456', '789def012', '345ghi678', '1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
+    numbers = get_first_and_last_numbers(strings).map(&:to_i)
+    sum = sum_numbers(numbers)
+    expect(sum).to eq(60)
+  end
+end
