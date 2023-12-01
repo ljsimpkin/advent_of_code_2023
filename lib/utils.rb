d77f1c5d1980ff8d092d79d70cf1202efd99d4f8
liam <ljsimpkin@gmail.com>
2023-12-02 09:08:20 +1300

    Added utility functions to lib/utils.rb.

---
+++ lib/utils.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'uri'
+require 'openssl'
+
+
+# get_input takes the day and returns the puzzle input for that day
+def get_input(day)  
+  urlstring = "https://adventofcode.com/2023/day/#{day}/input"
+  url = URI(urlstring)
+  
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  
+  request = Net::HTTP::Get.new(url)
+  request["cookie"] = ENV['AOC_COOKIE']
+  response = http.request(request)
+end
