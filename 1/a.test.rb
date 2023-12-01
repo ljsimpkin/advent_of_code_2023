4687bd2e8f6fbbb13f41cfb1bc81cdc824f7db3a
liam <ljsimpkin@gmail.com>
2023-12-01 19:44:10 +1300

    Refactor get_first_and_last_numbers test to use single digit numbers.

--- 1/a.test.rb
+++ 1/a.test.rb
@@ -9,7 +9,7 @@ end
 RSpec.describe 'get_first_and_last_numbers' do
   it 'returns the first and last number for each string' do
     input = ['123abc456', '789def012', '345ghi678']
-    output = [['123', '456'], ['789', '012'], ['345', '678']]
+    output = [['16'], ['72'], ['38']]
     expect(get_first_and_last_numbers(input)).to eq(output)
   end
 end
