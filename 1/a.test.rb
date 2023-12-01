a8311c2d21a1694af779973a6cbb48e5fccf9f5b
liam <ljsimpkin@gmail.com>
2023-12-01 20:46:23 +1300

    aider: Added a new test case for the function `add_website_to_string`.
    # Aider chat conversation:
    
    ASSISTANT: To create a test for a function that gets a website and adds it to a string, we need to modify `1/a.test.rb`:
    
    1. Add a new test case for the function `add_website_to_string`.
    
    Here is the *SEARCH/REPLACE* block:
    
    1/a.test.rb
    ```ruby
    <<<<<<< SEARCH
    =======
    RSpec.describe 'add_website_to_string' do
      it 'gets a website and adds it to a string' do
        website = 'www.example.com'
        string = 'This is a string. '
        output = 'This is a string. www.example.com'
        expect(add_website_to_string(string, website)).to eq(output)
      end
    end
    >>>>>>> REPLACE
    ```

--- 1/a.test.rb
+++ 1/a.test.rb
@@ -38,3 +38,11 @@ RSpec.describe 'get result' do
     expect(sum).to eq(142)
   end
 end
+RSpec.describe 'add_website_to_string' do
+  it 'gets a website and adds it to a string' do
+    website = 'www.example.com'
+    string = 'This is a string. '
+    output = 'This is a string. www.example.com'
+    expect(add_website_to_string(string, website)).to eq(output)
+  end
+end
