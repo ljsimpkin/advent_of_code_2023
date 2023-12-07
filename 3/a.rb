eebf95602927ed48a0a314b9f195bac21219da4c
liam <ljsimpkin@gmail.com>
2023-12-07 21:53:35 +1300

    aider: Implemented function `get_answer(puzzle)` that calculates the sum of numbers surrounding non-empty cells in a puzzle.
    # Aider chat conversation:
    
    ASSISTANT: To implement this, we need to modify `3/a.rb`:
    
    1. Define a new function `get_answer(puzzle)` that takes a string representation of a puzzle as input.
    2. Split the input string into an array of strings, each representing a row of the puzzle.
    3. Iterate over each row and each character in the row.
    4. If the character is not a '.', check the characters around it and sum up the numbers.
    5. Return the total sum.
    
    Here is the *SEARCH/REPLACE* block:
    
    3/a.rb
    ```ruby
    <<<<<<< SEARCH
    def get_answer(puzzle)
    
    end
    =======
    def get_answer(puzzle)
      rows = puzzle.split("\n")
      sum = 0
    
      rows.each_with_index do |row, i|
        row.chars.each_with_index do |char, j|
          next if char == '.'
    
          # Check the characters around the current character
          [-1, 0, 1].each do |di|
            [-1, 0, 1].each do |dj|
              next if di == 0 && dj == 0
              ni, nj = i + di, j + dj
    
              # Check if the new indices are within the bounds of the puzzle
              next if ni < 0 || ni >= rows.size || nj < 0 || nj >= row.size
    
              # Add the number to the sum if it's a number
              sum += rows[ni][nj].to_i if rows[ni][nj] =~ /\d/
            end
          end
        end
      end
    
      sum
    end
    >>>>>>> REPLACE
    ```

--- 3/a.rb
+++ 3/a.rb
@@ -1,3 +1,26 @@
 def get_answer(puzzle)
-  
+  rows = puzzle.split("\n")
+  sum = 0
+
+  rows.each_with_index do |row, i|
+    row.chars.each_with_index do |char, j|
+      next if char == '.'
+
+      # Check the characters around the current character
+      [-1, 0, 1].each do |di|
+        [-1, 0, 1].each do |dj|
+          next if di == 0 && dj == 0
+          ni, nj = i + di, j + dj
+
+          # Check if the new indices are within the bounds of the puzzle
+          next if ni < 0 || ni >= rows.size || nj < 0 || nj >= row.size
+
+          # Add the number to the sum if it's a number
+          sum += rows[ni][nj].to_i if rows[ni][nj] =~ /\d/
+        end
+      end
+    end
+  end
+
+  sum
 end
