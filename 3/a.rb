bc9fb2f1e3bc8e0ac1ac58106aa85e6e1d1d23df
liam <ljsimpkin@gmail.com>
2023-12-07 22:02:01 +1300

    aider: Replace variables `i` and `j` with `row_index` and `col_index`, and replace variables `ni` and `nj` with `new_row_index` and `new_col_index`.
    # Aider chat conversation:
    
    USER: label the variables with longer names than i and j to describe which index they are referring to
    
    ASSISTANT: To make this change we need to modify `3/a.rb`:
    
    1. Replace the variables `i` and `j` with `row_index` and `col_index` respectively.
    2. Replace the variables `ni` and `nj` with `new_row_index` and `new_col_index` respectively.
    
    Here is the *SEARCH/REPLACE* block:
    
    3/a.rb
    ```ruby
    <<<<<<< SEARCH
      rows.each_with_index do |row, i|
        row.chars.each_with_index do |char, j|
          next if char == '.'
    
          # Check the characters around the current character
          (-1..1).each do |d|
            ni, nj = (i + d) % rows.size, (j + d) % row.size
    
            # Add the number to the sum if it's a number
            sum += rows[ni][nj].to_i if rows[ni][nj] =~ /\d/
          end
        end
      end
    =======
      rows.each_with_index do |row, row_index|
        row.chars.each_with_index do |char, col_index|
          next if char == '.'
    
          # Check the characters around the current character
          (-1..1).each do |d|
            new_row_index, new_col_index = (row_index + d) % rows.size, (col_index + d) % row.size
    
            # Add the number to the sum if it's a number
            sum += rows[new_row_index][new_col_index].to_i if rows[new_row_index][new_col_index] =~ /\d/
          end
        end
      end
    >>>>>>> REPLACE
    ```

--- 3/a.rb
+++ 3/a.rb
@@ -2,16 +2,16 @@ def get_answer(puzzle)
   rows = puzzle.split("\n")
   sum = 0
 
-  rows.each_with_index do |row, i|
-    row.chars.each_with_index do |char, j|
+  rows.each_with_index do |row, row_index|
+    row.chars.each_with_index do |char, col_index|
       next if char == '.'
 
       # Check the characters around the current character
       (-1..1).each do |d|
-        ni, nj = (i + d) % rows.size, (j + d) % row.size
+        new_row_index, new_col_index = (row_index + d) % rows.size, (col_index + d) % row.size
 
         # Add the number to the sum if it's a number
-        sum += rows[ni][nj].to_i if rows[ni][nj] =~ /\d/
+        sum += rows[new_row_index][new_col_index].to_i if rows[new_row_index][new_col_index] =~ /\d/
       end
     end
   end
