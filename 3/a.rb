9e1a2310cbc8fe3a92bbd3335238fb0380a5ed28
liam <ljsimpkin@gmail.com>
2023-12-07 22:09:56 +1300

    aider: Modified `get_answer` function to parse the puzzle into a 2D array, iterate over each cell, and add the number to the sum if any of its 8 neighbors is a symbol.
    # Aider chat conversation:
    
    ASSISTANT: To solve this problem, we need to modify `3/a.rb` to:
    
    1. Parse the input puzzle into a 2D array.
    2. Iterate over each cell in the array.
    3. If a cell is a number and any of its 8 neighbors is a symbol, add the number to the sum.
    4. Return the sum.
    
    Here is the *SEARCH/REPLACE* block:
    
    3/a.rb
    ```ruby
    <<<<<<< SEARCH
    def get_answer(puzzle)
    end
    =======
    def get_answer(puzzle)
      # Parse the puzzle into a 2D array
      grid = puzzle.split("\n").map { |row| row.split('') }
    
      # Initialize the sum
      sum = 0
    
      # Iterate over each cell in the grid
      grid.each_with_index do |row, i|
        row.each_with_index do |cell, j|
          # If the cell is a number
          if cell =~ /\d/
            # Check the 8 neighbors
            neighbors = [
              [i-1, j-1], [i-1, j], [i-1, j+1],
              [i, j-1],             [i, j+1],
              [i+1, j-1], [i+1, j], [i+1, j+1]
            ]
            neighbors.each do |x, y|
              # If the neighbor is a symbol, add the number to the sum
              if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] =~ /[^.\d]/
                sum += cell.to_i
                break
              end
            end
          end
        end
      end
    
      # Return the sum
      sum
    end
    >>>>>>> REPLACE
    ```
    
    This code should solve the problem as described. It parses the input puzzle into a 2D array, then iterates over each cell in the array. If a cell is a number and any of its 8 neighbors is a symbol, it adds the number to the sum. Finally, it returns the sum.

--- 3/a.rb
+++ 3/a.rb
@@ -1,2 +1,32 @@
 def get_answer(puzzle)
+  # Parse the puzzle into a 2D array
+  grid = puzzle.split("\n").map { |row| row.split('') }
+
+  # Initialize the sum
+  sum = 0
+
+  # Iterate over each cell in the grid
+  grid.each_with_index do |row, i|
+    row.each_with_index do |cell, j|
+      # If the cell is a number
+      if cell =~ /\d/
+        # Check the 8 neighbors
+        neighbors = [
+          [i-1, j-1], [i-1, j], [i-1, j+1],
+          [i, j-1],             [i, j+1],
+          [i+1, j-1], [i+1, j], [i+1, j+1]
+        ]
+        neighbors.each do |x, y|
+          # If the neighbor is a symbol, add the number to the sum
+          if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] =~ /[^.\d]/
+            sum += cell.to_i
+            break
+          end
+        end
+      end
+    end
+  end
+
+  # Return the sum
+  sum
 end
