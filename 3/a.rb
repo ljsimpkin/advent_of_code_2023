c97cc4b1b54ef2294025f00711283af8c7e01eaf
liam <ljsimpkin@gmail.com>
2023-12-07 22:04:14 +1300

    Refactor get_answer method to remove unnecessary code.

--- 3/a.rb
+++ 3/a.rb
@@ -1,20 +1,3 @@
 def get_answer(puzzle)
-  rows = puzzle.split("\n")
-  sum = 0
-
-  rows.each_with_index do |row, row_index|
-    row.chars.each_with_index do |char, col_index|
-      next if char == '.'
-
-      # Check the characters around the current character
-      (-1..1).each do |d|
-        new_row_index, new_col_index = (row_index + d) % rows.size, (col_index + d) % row.size
-
-        # Add the number to the sum if it's a number
-        sum += rows[new_row_index][new_col_index].to_i if rows[new_row_index][new_col_index] =~ /\d/
-      end
-    end
-  end
-
-  sum
+  
 end
