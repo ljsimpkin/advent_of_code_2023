f2d4e75011523c2821057d71653f473356dc083d
liam <ljsimpkin@gmail.com>
2023-12-07 22:07:28 +1300

    aider: Implemented the `get_answer(puzzle)` function to iterate through the puzzle string, split it into lines, and then iterate through each character in each line, adding any adjacent numbers to the sum.
    # Aider chat conversation:
    
    ASSISTANT: To solve this problem, we need to modify `3/a.rb`:
    
    1. Implement the `get_answer(puzzle)` function to iterate through the puzzle string, split it into lines, and then iterate through each character in each line. If the character is a number and it is adjacent to a symbol, add it to the sum.
    
    Here is the *SEARCH/REPLACE* block:
    
    3/a.rb
    ```ruby
    <<<<<<< SEARCH
    def get_answer(puzzle)
    end
    =======
    def get_answer(puzzle)
      sum = 0
      rows = puzzle.split("\n")
      rows.each_with_index do |row, i|
        row.chars.each_with_index do |char, j|
          if char =~ /\d/ && adjacent_to_symbol?(rows, i, j)
            sum += char.to_i
          end
        end
      end
      sum
    end
    
    def adjacent_to_symbol?(rows, i, j)
      [-1, 0, 1].product([-1, 0, 1]).any? do |di, dj|
        next if di == 0 && dj == 0
        ni, nj = i + di, j + dj
        next if ni < 0 || nj < 0 || ni >= rows.size || nj >= rows.first.size
        rows[ni][nj] =~ /[^.\d]/
      end
    end
    >>>>>>> REPLACE
    ```
    
    This solution assumes that the symbols are any characters other than digits and periods. If the symbols are specifically defined, you may need to adjust the regular expression in the `adjacent_to_symbol?` method.

--- 3/a.rb
+++ 3/a.rb
@@ -1,2 +1,21 @@
 def get_answer(puzzle)
+  sum = 0
+  rows = puzzle.split("\n")
+  rows.each_with_index do |row, i|
+    row.chars.each_with_index do |char, j|
+      if char =~ /\d/ && adjacent_to_symbol?(rows, i, j)
+        sum += char.to_i
+      end
+    end
+  end
+  sum
+end
+
+def adjacent_to_symbol?(rows, i, j)
+  [-1, 0, 1].product([-1, 0, 1]).any? do |di, dj|
+    next if di == 0 && dj == 0
+    ni, nj = i + di, j + dj
+    next if ni < 0 || nj < 0 || ni >= rows.size || nj >= rows.first.size
+    rows[ni][nj] =~ /[^.\d]/
+  end
 end
