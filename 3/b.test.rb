44d38f1454b60f9bfb22b7f3aa4a3b538c003626
liam <ljsimpkin@gmail.com>
2023-12-14 22:55:05 +1300

    wip for 3b

--- 3/b.test.rb
+++ 3/b.test.rb
@@ -1,11 +1,11 @@
-require_relative 'a.rb'
+require_relative 'b.rb'
 
 RSpec.describe 'True' do
   it 'equals true' do
     expect(true).to eq(true)
   end
 
-  it 'get_answer multiplies all the numbers next to a gear *' do
+  it 'get_answer multiplies all the numbers next to a gear * and then retuns the sum' do
     puzzle = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
     expect(get_answer(puzzle)).to eq(467835)
   end
