ac85870bf5f2ae8711ac4a97cb6fd28d166b4f88
liam <ljsimpkin@gmail.com>
2023-12-14 22:40:20 +1300

    aider: Change the sum operation to multiplication when a gear symbol '*' is encountered and initialize the product to 1 instead of 0.
    # Aider chat conversation:
    
    ASSISTANT: To implement this change, we need to modify `3/b.rb`:
    
    1. Change the sum operation to multiplication when a gear symbol '*' is encountered.
    2. Initialize the product to 1 instead of 0.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    3/b.rb
    ```ruby
    <<<<<<< SEARCH
      # Initialize the sum
      sum = 0
    =======
      # Initialize the product
      product = 1
    >>>>>>> REPLACE
    ```
    
    3/b.rb
    ```ruby
    <<<<<<< SEARCH
              # If the neighbor is a symbol and the number has not been counted
              if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] =~ /[^.\d]/ && !counted.include?(number)
                # Add the number to the sum
                sum += number.to_i
                # Mark the number as counted
                counted << number
                break
              end
    =======
              # If the neighbor is a gear symbol and the number has not been counted
              if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] == '*' && !counted.include?(number)
                # Multiply the product by the number
                product *= number.to_i
                # Mark the number as counted
                counted << number
                break
              end
    >>>>>>> REPLACE
    ```
    
    3/b.rb
    ```ruby
    <<<<<<< SEARCH
      # Return the sum
      sum
    =======
      # Return the product
      product
    >>>>>>> REPLACE
    ```
    
    Also, we need to modify the test file `3/b.test.rb` to reflect this change:
    
    3/b.test.rb
    ```ruby
    <<<<<<< SEARCH
      it 'get_answer sums all the multpiles of numbers next to a gear *' do
        puzzle = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
        expect(get_answer(puzzle)).to eq(467835)
      end
    =======
      it 'get_answer multiplies all the numbers next to a gear *' do
        puzzle = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
        expect(get_answer(puzzle)).to eq(467835)
      end
    >>>>>>> REPLACE
    ```

--- 3/b.rb
+++ 3/b.rb
@@ -5,8 +5,8 @@ def get_answer(puzzle = get_input("3").body)
   # Parse the puzzle into a 2D array
   grid = puzzle.split("\n").map { |row| row.split('') }
 
-  # Initialize the sum
-  sum = 0
+  # Initialize the product
+  product = 1
 
   # Initialize the counted numbers
   counted = []
@@ -40,10 +40,10 @@ def get_answer(puzzle = get_input("3").body)
           [i+1, j-1], [i+1, j], [i+1, j+1]
         ]
         neighbors.each do |x, y|
-          # If the neighbor is a symbol and the number has not been counted
-          if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] =~ /[^.\d]/ && !counted.include?(number)
-            # Add the number to the sum
-            sum += number.to_i
+          # If the neighbor is a gear symbol and the number has not been counted
+          if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] == '*' && !counted.include?(number)
+            # Multiply the product by the number
+            product *= number.to_i
             # Mark the number as counted
             counted << number
             break
@@ -56,8 +56,8 @@ def get_answer(puzzle = get_input("3").body)
     end
   end
 
-  # Return the sum
-  sum
+  # Return the product
+  product
 end
 
 puts get_answer()
--- 3/b.test.rb
+++ 3/b.test.rb
@@ -5,7 +5,7 @@ RSpec.describe 'True' do
     expect(true).to eq(true)
   end
 
-  it 'get_answer sums all the multpiles of numbers next to a gear *' do
+  it 'get_answer multiplies all the numbers next to a gear *' do
     puzzle = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
     expect(get_answer(puzzle)).to eq(467835)
   end
