74179a5bd00b9e93a642d00d5681dd92e5e13c53
liam <ljsimpkin@gmail.com>
2023-12-14 22:46:24 +1300

    aider: Add gear_found flag to keep track of whether a gear has been encountered and modify multiplication logic accordingly.
    # Aider chat conversation:
    
    USER: someimes the index will meet another group_product before finsihing the multiplication of one group
    
    ASSISTANT: To handle this situation, we need to modify `3/b.rb`:
    
    1. Add a new variable `gear_found` to keep track of whether a gear has been encountered.
    2. Set `gear_found` to true whenever a gear is encountered.
    3. Only multiply `group_product` by the number if `gear_found` is true.
    4. Reset `gear_found` to false and add `group_product` to `product` whenever a dot is encountered or the end of the grid is reached.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    3/b.rb
    ```ruby
    <<<<<<< SEARCH
      # Initialize the total product and group product
      product = 0
      group_product = 1
    =======
      # Initialize the total product, group product and gear found flag
      product = 0
      group_product = 1
      gear_found = false
    >>>>>>> REPLACE
    ```
    
    3/b.rb
    ```ruby
    <<<<<<< SEARCH
            neighbors.each do |x, y|
              # If the neighbor is a gear symbol and the number has not been counted
              if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] == '*' && !counted.include?(number)
                # Multiply the group product by the number
                group_product *= number.to_i
                # Mark the number as counted
                counted << number
                break
              end
            end
    =======
            if gear_found && !counted.include?(number)
              # Multiply the group product by the number
              group_product *= number.to_i
              # Mark the number as counted
              counted << number
            end
    
            neighbors.each do |x, y|
              # If the neighbor is a gear symbol
              if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] == '*'
                # Set gear found flag to true
                gear_found = true
                break
              end
            end
    >>>>>>> REPLACE
    ```
    
    3/b.rb
    ```ruby
    <<<<<<< SEARCH
          elsif cell == '.' || i == grid.size-1 && j == row.size-1
            # Add the group product to the total product
            product += group_product
            # Reset the group product and the counted numbers
            group_product = 1
            counted = []
          end
    =======
          if cell == '.' || i == grid.size-1 && j == row.size-1
            if gear_found
              # Add the group product to the total product
              product += group_product
              # Reset the group product and the gear found flag
              group_product = 1
              gear_found = false
            end
            # Reset the counted numbers
            counted = []
          end
    >>>>>>> REPLACE
    ```

--- 3/b.rb
+++ 3/b.rb
@@ -5,9 +5,10 @@ def get_answer(puzzle = get_input("3").body)
   # Parse the puzzle into a 2D array
   grid = puzzle.split("\n").map { |row| row.split('') }
 
-  # Initialize the total product and group product
+  # Initialize the total product, group product and gear found flag
   product = 0
   group_product = 1
+  gear_found = false
 
   # Initialize the counted numbers
   counted = []
@@ -40,21 +41,30 @@ def get_answer(puzzle = get_input("3").body)
           [i, j-1],               [i, j+1],
           [i+1, j-1], [i+1, j], [i+1, j+1]
         ]
+        if gear_found && !counted.include?(number)
+          # Multiply the group product by the number
+          group_product *= number.to_i
+          # Mark the number as counted
+          counted << number
+        end
+
         neighbors.each do |x, y|
-          # If the neighbor is a gear symbol and the number has not been counted
-          if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] == '*' && !counted.include?(number)
-            # Multiply the group product by the number
-            group_product *= number.to_i
-            # Mark the number as counted
-            counted << number
+          # If the neighbor is a gear symbol
+          if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] == '*'
+            # Set gear found flag to true
+            gear_found = true
             break
           end
         end
-      elsif cell == '.' || i == grid.size-1 && j == row.size-1
-        # Add the group product to the total product
-        product += group_product
-        # Reset the group product and the counted numbers
-        group_product = 1
+      if cell == '.' || i == grid.size-1 && j == row.size-1
+        if gear_found
+          # Add the group product to the total product
+          product += group_product
+          # Reset the group product and the gear found flag
+          group_product = 1
+          gear_found = false
+        end
+        # Reset the counted numbers
         counted = []
       end
     end
