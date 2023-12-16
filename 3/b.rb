0f3e569cf9ee6b21977984053482103ff72b2cc7
liam <ljsimpkin@gmail.com>
2023-12-16 17:39:43 +1300

    all passing tests

--- 3/b.rb
+++ 3/b.rb
@@ -56,8 +56,8 @@ def get_answer(puzzle = get_input("3").body)
           [i+1, j-1], [i+1, j], [i+1, j+1]
         ]
         neighbors.each do |x, y|
-          # If the neighbor is a * and the number has not been counted
-          if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] == '*' && !counted.include?(number)
+          # If the neighbor is a * and the number has not been counted for that symbol
+          if x.between?(0, grid.size-1) && y.between?(0, row.size-1) && grid[x][y] == '*' && !counted.include?("#{x}#{y}")
             # Add the number to the hash {[i,j] => [x,y,z]}
             if gear_number_neighbors["#{x}#{y}"]
               gear_number_neighbors["#{x}#{y}"].push(number)
@@ -66,11 +66,10 @@ def get_answer(puzzle = get_input("3").body)
             end
             sum += number.to_i
             # Mark the number as counted
-            counted << number
-            break
+            counted << "#{x}#{y}"
           end
         end
-      elsif cell =~ /[^.\d]/
+      else # if cell is not a number
         # Reset the counted numbers
         counted = []
       end
@@ -82,4 +81,4 @@ def get_answer(puzzle = get_input("3").body)
   multiply_and_sum(gear_number_neighbors)
 end
 
-puts get_answer()
+# puts get_answer()
