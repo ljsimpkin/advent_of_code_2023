be6d884fb23b33ce8834dc7d557ac5495b5bdcb2
liam <ljsimpkin@gmail.com>
2023-12-22 10:18:19 +1300

    aider: Update parse_puzzle function to split input string into lines and create a hash for each line.
    # Aider chat conversation:
    
    ASSISTANT: To make this change we need to modify `4/a.rb`:
    
    1. Update the `parse_puzzle` function to split the input string into lines, then for each line, split it into a card name and two sets of numbers. The card name and sets of numbers are then used to create a hash which is added to the result.
    
    Here is the *SEARCH/REPLACE* block:
    
    4/a.rb
    ```ruby
    <<<<<<< SEARCH
    def parse_puzzle(puzzle_input)
    end
    =======
    def parse_puzzle(puzzle_input)
      result = {}
      puzzle_input.split("\n").each do |line|
        card_name, numbers = line.split(": ")
        card_name.gsub!(" ", "_")
        set1, set2 = numbers.split(" | ")
        result[card_name] = {set1 => set2}
      end
      result
    end
    >>>>>>> REPLACE
    ```

--- 4/a.rb
+++ 4/a.rb
@@ -1,5 +1,13 @@
 
 def parse_puzzle(puzzle_input)
+  result = {}
+  puzzle_input.split("\n").each do |line|
+    card_name, numbers = line.split(": ")
+    card_name.gsub!(" ", "_")
+    set1, set2 = numbers.split(" | ")
+    result[card_name] = {set1 => set2}
+  end
+  result
 end
 
 def calc_score(card)
