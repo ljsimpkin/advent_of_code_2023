42ee78d30b07850c61f075fe87771f690e21e7d0
liam <ljsimpkin@gmail.com>
2024-03-09 12:38:49 +1300

    aider: Update templates to get the 'PUZZLE_DAY' from the parent directory name.
    # Aider chat conversation:
    
    USER: awesome, make the templates get the 'PUZZLE_DAY' from the parent directory name
    
    ASSISTANT: To make this change we need to modify `lib/template/a.rb` and `lib/template/b.rb` to:
    
    1. Get the 'PUZZLE_DAY' from the parent directory name.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    lib/template/a.rb
    ```ruby
    <<<<<<< SEARCH
    def get_answer_a(puzzle_input = get_puzzle(PUZZLE_DAY))
    =======
    def get_answer_a(puzzle_input = get_puzzle(File.basename(Dir.getwd)))
    >>>>>>> REPLACE
    ```
    
    lib/template/b.rb
    ```ruby
    <<<<<<< SEARCH
    def get_answer_b(puzzle_input = get_puzzle(PUZZLE_DAY))
    =======
    def get_answer_b(puzzle_input = get_puzzle(File.basename(Dir.getwd)))
    >>>>>>> REPLACE
    ```

--- lib/template/a.rb
+++ lib/template/a.rb
@@ -1,4 +1,4 @@
 require_relative "../lib/utils.rb"
 
-def get_answer_a(puzzle_input = get_puzzle(PUZZLE_DAY))
+def get_answer_a(puzzle_input = get_puzzle(File.basename(Dir.getwd)))
 end
--- lib/template/b.rb
+++ lib/template/b.rb
@@ -1,4 +1,4 @@
 require_relative "a.rb"
 
-def get_answer_b(puzzle_input = get_puzzle(PUZZLE_DAY))
+def get_answer_b(puzzle_input = get_puzzle(File.basename(Dir.getwd)))
 end
