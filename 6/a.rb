0d5338e85aa2225018c08617ccd9f5413d1cae1b
liam <ljsimpkin@gmail.com>
2024-03-10 11:31:47 +1300

    puzzle complete for 6a

--- 6/a.rb
+++ 6/a.rb
@@ -15,5 +15,22 @@ def parse_puzzle(puzzle_input)
   puzzle
 end
 
-def get_answer_a(puzzle_input = get_puzzle(6))
+def get_distances(time)
+  distances = []
+  (0..time).each do |i|
+    distances.push(i * (time - i))
+  end
+  return distances
 end
+
+def number_of_ways_you_can_win(time, record)
+  distances = get_distances(time)
+  distances.count { |value| value > record }
+end
+
+def get_answer_a(puzzle_input = get_puzzle(6))
+  puzzle = parse_puzzle(puzzle_input)
+  puzzle.map { |h| number_of_ways_you_can_win(h["Time"], h["Distance"]) }.inject(:*)
+end
+
+print get_answer_a()
--- 6/a.test.rb
+++ 6/a.test.rb
@@ -12,8 +12,27 @@ RSpec.describe 'parse_puzzle' do
   end
 end
 
+RSpec.describe 'get_distances' do
+  it 'takes a time and returns all possible distances' do
+    expect(get_distances(7)).to eq([0,6,10,12,12,10,6,0])
+  end
+end
+
+RSpec.describe 'number_of_ways_you_can_win' do
+  it 'returns the total number of ways you can win' do
+    expect(number_of_ways_you_can_win(7, 9)).to eq(4)
+  end
+end
+
 RSpec.describe 'get_answer' do
   xit 'returns the answer' do
+    expect(get_answer_a(puzzle_demo)).to eq(288)
+  end
+end
+
+
+RSpec.describe 'get_answer' do
+  it 'returns the answer' do
     expect(get_answer_a(puzzle_demo)).to eq(288)
   end
 end
