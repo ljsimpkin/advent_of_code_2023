188e9bcc3fba1c9ea3e864137037a800c33c45a9
liam <ljsimpkin@gmail.com>
2024-03-10 12:15:53 +1300

    aider: Refactored get_hand_ranking to translate poker hands to hand types.
    # Aider chat conversation:
    
    USER: make the function get_hand_ranking translate a poker hand of 5 characters and map it to the type of hand. For example QQQJA maps to THREE_OF_A_KIND
    
    ASSISTANT: To implement the `get_hand_ranking` function, we need to modify `7/a.rb`:
    
    1. Replace the existing `get_hand_ranking` function with a new implementation that translates a poker hand into the type of hand.
    
    Here is the *SEARCH/REPLACE* block:
    
    7/a.rb
    ```ruby
    <<<<<<< SEARCH
    def get_hand_ranking(hand)
    end
    =======
    def get_hand_ranking(hand)
      counts = hand.chars.group_by(&:itself).transform_values(&:count)
      case counts.values.sort
      when [1, 4] then FOUR_OF_A_KIND
      when [2, 3] then FULL_HOUSE
      when [1, 1, 3] then THREE_OF_A_KIND
      when [1, 2, 2] then TWO_PAIR
      when [2, 3] then ONE_PAIR
      else HIGH_CARD
      end
    end
    >>>>>>> REPLACE
    ```
    
    This new implementation of `get_hand_ranking` function counts the occurrences of each character in the hand, groups them by count, and then uses a case statement to determine the type of hand based on the counts.

--- 7/a.rb
+++ 7/a.rb
@@ -9,6 +9,15 @@ ONE_PAIR = 2
 HIGH_CARD = 1
 
 def get_hand_ranking(hand)
+  counts = hand.chars.group_by(&:itself).transform_values(&:count)
+  case counts.values.sort
+  when [1, 4] then FOUR_OF_A_KIND
+  when [2, 3] then FULL_HOUSE
+  when [1, 1, 3] then THREE_OF_A_KIND
+  when [1, 2, 2] then TWO_PAIR
+  when [2, 3] then ONE_PAIR
+  else HIGH_CARD
+  end
 end
 
 def get_answer_a(puzzle_input = get_puzzle(7))
