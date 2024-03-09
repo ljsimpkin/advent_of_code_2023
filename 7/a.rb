07aedb25f8cc2504491de646e1399f444dfa6b62
liam <ljsimpkin@gmail.com>
2024-03-10 12:47:16 +1300

    Add parsing and comparison functions for poker hands.

--- 7/a.rb
+++ 7/a.rb
@@ -8,6 +8,21 @@ TWO_PAIR = 3
 ONE_PAIR = 2
 HIGH_CARD = 1
 
+def parse_puzzle(puzzle_input)
+  puzzle_input.split("\n").map { |line| line.split(' ') }
+end
+
+def get_card_strength(card)
+  case card
+  when 'T' then 10
+  when 'J' then 11
+  when 'Q' then 12
+  when 'K' then 13
+  when 'A' then 14
+  else card.to_i
+  end
+end
+
 def get_hand_ranking(hand)
   counts = hand.chars.group_by(&:itself).transform_values(&:count)
   case counts.values.sort
@@ -20,6 +35,36 @@ def get_hand_ranking(hand)
   end
 end
 
+# Compares two poker hands and determines the winner.
+#
+# @param hand1 [String] The first poker hand.
+# @param hand2 [String] The second poker hand.
+# @return [Boolean] Returns true if hand1 wins, false if hand2 wins, and nil if it's a tie.
+def compare_hands(hand1, hand2)
+  ranking1 = get_hand_ranking(hand1)
+  ranking2 = get_hand_ranking(hand2)
+
+  # If the hand rankings are different, the hand with the higher ranking wins.
+  return ranking1 > ranking2 if ranking1 != ranking2
+
+  # If the hand rankings are the same, compare the card strengths.
+  strength1 = hand1.chars.map { |card| get_card_strength(card) }.sort.reverse
+  strength2 = hand2.chars.map { |card| get_card_strength(card) }.sort.reverse
+
+  # Compare the card strengths from highest to lowest.
+  strength1.each_with_index do |card1, i|
+    card2 = strength2[i]
+    return true if card1 > card2
+    return false if card1 < card2
+  end
+
+  # If all card strengths are equal, it's a tie.
+  nil
+end
+
+def sort_hands(hands)
+end
+
 def get_answer_a(puzzle_input = get_puzzle(7))
-  6440
+  
 end
