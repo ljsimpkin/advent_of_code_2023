78a84d55a1df1e752861b01c36857488b8cfd8de
liam <ljsimpkin@gmail.com>
2024-03-09 13:09:39 +1300

    update templates with new way to get day

--- lib/template/a.rb
+++ lib/template/a.rb
@@ -1,5 +1,4 @@
 require_relative "../lib/utils.rb"
-PUZZLE_DAY = File.basename(Dir.getwd)
 
 def get_answer_a(puzzle_input = get_puzzle(PUZZLE_DAY))
 end
--- lib/template/b.rb
+++ lib/template/b.rb
@@ -1,5 +1,4 @@
 require_relative "a.rb"
-PUZZLE_DAY = File.basename(Dir.getwd)
 
 def get_answer_b(puzzle_input = get_puzzle(PUZZLE_DAY))
 end
