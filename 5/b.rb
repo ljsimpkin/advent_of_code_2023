4a3059005f13d9e3185f1e94eb2306b676aae89b
liam <ljsimpkin@gmail.com>
2024-03-09 15:56:39 +1300

    working slow solution for 5b

--- 5/b.rb
+++ 5/b.rb
@@ -1,4 +1,32 @@
 require_relative "a.rb"
 
-def get_answer_b(puzzle_input = get_puzzle(5))
+def get_seeds(seed_map)
+  all_seeds = []
+
+  seed_map.each_with_index do |seed, index|
+    if index.even?
+      all_seeds.push(seed)
+      # byebug
+      (1..(seed_map[index + 1] - 1)).each do |i|
+        all_seeds.push(seed + i)
+      end
+    end
+  end
+
+  all_seeds
 end
+
+def get_answer_b(puzzle_input = get_puzzle(5))
+  puzzle = parse_puzzle(puzzle_input)
+  locations = []
+
+  all_seeds = get_seeds(puzzle["seeds"])
+
+  all_seeds.each do |seed|
+    locations.push(seed_to_location(puzzle, seed))
+  end
+
+  locations.min
+end
+
+print get_answer_b
--- 5/b.test.rb
+++ 5/b.test.rb
@@ -1,9 +1,48 @@
 require_relative "b.rb"
 
-puzzle_demo = ""
+puzzle_demo = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"
+
+RSpec.describe 'get_aseeds' do
+  it 'returns the seedsr' do
+    seeds = [2, 4, 8, 2]
+    expect(get_seeds(seeds)).to eq([2,3,4,5,8,9])
+  end
+end
 
 RSpec.describe 'get_answer_b' do
   it 'returns the answer' do
-    expect(get_answer_b(puzzle_demo)).to eq(true)
+    expect(get_answer_b(puzzle_demo)).to eq(46)
   end
 end
