cef3a3817830870bb2d3efe21e023f62770f7fdf
liam <ljsimpkin@gmail.com>
2024-03-10 12:15:52 +1300

    Add 7/a.rb file.

---
+++ 7/a.rb
@@ -0,0 +1,16 @@
+require_relative "../lib/utils.rb"
+
+FIVE_OF_A_KIND = 7
+FOUR_OF_A_KIND = 6
+FULL_HOUSE = 5
+THREE_OF_A_KIND = 4
+TWO_PAIR = 3
+ONE_PAIR = 2
+HIGH_CARD = 1
+
+def get_hand_ranking(hand)
+end
+
+def get_answer_a(puzzle_input = get_puzzle(7))
+  6440
+end
